989551790fc9f8d27ffb996adcd6c440a2fc1e0a
João Teves <68256316+Teves-web@users.noreply.github.com>
2022-09-08 23:26:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,27 @@ function contar(){
     if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.lengh == 0) {
         window.alert('[ERRO] Faltam dados!!!')
     }else{
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando Passo 1')
+            p = 1
+        }
+        if (i <f ) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //contagem regressiva
 
         for(var c = i; c <= f; c += p){
             res.innerHTML += `${c}`
+        for(let c = i; c <= f; c += p){
+            res.innerHTML += ` ${c} \u{1F449}`
         }
     }
+    res.innerHTML += ``
 }
